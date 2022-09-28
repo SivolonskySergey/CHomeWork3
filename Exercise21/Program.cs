493f8b9f555aae40765838e3319864a1a4b51a48
Sergey <bibert.shmibert@gmail.com>
2022-09-28 17:03:14 +0400

    add exercise 21

---
+++ Exercise21/Program.cs
@@ -0,0 +1,15 @@
+﻿
+void Distance(int[] firstDot, int[] secondDot) 
+{
+    double summ = 0;
+    for(int i = 0; i < 3; i++)
+    {
+        summ = summ + Math.Pow((secondDot[i] - firstDot[i]), 2);
+    }
+    double result = Math.Sqrt(summ);
+    Console.WriteLine(result);
+}
+
+int[] first = {7, -5, 0};
+int[] second = {1, -1, 9};
+Distance(first, second);
