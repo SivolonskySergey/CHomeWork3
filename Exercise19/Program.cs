51c38ba071b451a0811ad27f203c1aa19e977fe9
Sergey <bibert.shmibert@gmail.com>
2022-09-28 15:20:56 +0400

    add exercise 19

---
+++ Exercise19/Program.cs
@@ -0,0 +1,24 @@
+﻿
+void isPalindrom(int number) 
+{
+   int absNumber = Math.Abs(number);
+   string numToString = Convert.ToString(absNumber);
+
+   if ( absNumber > 99999 || absNumber < 10000 )
+   {
+      Console.WriteLine("Неверная длина числа, требуется пятизначное!");
+   }
+   else if ( $"{numToString[1]}{numToString[0]}" == $"{numToString[3]}{numToString[4]}" ) 
+   {
+      Console.WriteLine($"{number} - является палиндромом");
+   }
+   else {
+      Console.WriteLine($"{number} - не является палиндромом");
+   }
+}
+
+isPalindrom(-23432);
+isPalindrom(90981);
+isPalindrom(14212);
+isPalindrom(3433);
+isPalindrom(-1001);
